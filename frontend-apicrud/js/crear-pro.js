4807ca452b5f7a065b3ad485aad5d2b8ff217bcc
isaac <castanedaisaac655@gmail.com>
2026-03-04 22:10:13 -0500

    se termino el http

--- frontend-apicrud/js/crear-pro.js
+++ frontend-apicrud/js/crear-pro.js
@@ -2,11 +2,25 @@
  const d = document;
  let nameInput= d.querySelector('#productos-select');
  let princeInput= d.querySelector ('#precio-pro');
-
   let stockInput = d.querySelector ('#stock-pro');
  let descripcionInput=d.querySelector ('#des-pro');
  let imagen = d.querySelector('#imagen-pro');
+ let productUpdate;
  let btnCreate= d.querySelector ('#btn-create');
+  let nameUser = d.querySelector("#nombre-usuario");
+ let btnLogout = d.querySelector("#btnLogout");
+
+  //Funcion para poner el nombre del usuario
+ let getUser = () => {
+   let user = JSON.parse(localStorage.getItem("userLogin"));
+   nameUser.textContent = user.nombre;
+ }
+//evento para el boton del logout
+ btnLogout.addEventListener("click",() =>{ 
+    localStorage.removeItem("userLogin");
+    location.href = "../login.html";
+ });
+
 
  //evento al botón del formulario 
  btnCreate.addEventListener ('click',()=>{
@@ -14,6 +28,20 @@
  let dataProduct=getDataproduct();
  senDataProduct(dataProduct)
     });
+
+    //evento al navegador para compobrar si hay recagrdo la pagina
+     d.addEventListener("DOMContentLoaded",()=>{
+      getUser();
+productUpdate= JSON.parse(localStorage.getItem("productEdit"));
+
+ if(productUpdate != null){
+
+}
+    });
+
+ updateDataProduct();
+
+
 
 // función para validar el formulario y
  //obtener los datos del formulario
@@ -63,3 +91,71 @@ let sendDataProduct = async (data) => {
    console.log(error);
  }
 };
+
+ //función para editar el producto
+
+ let updateDataProduct=()=>{
+
+//agregar datos a editar en los campos del dormulario
+
+ nameInput.value= productUpdate.nombre;
+ precioInput.value=productUpdate.precio;
+ stockInput.value= productUpdate.stock;
+ descripcionInput.value= productUpdate.descripcion;
+ imagen.src=productUpdate.imagen;
+
+ let product; 
+
+ //alternar el botón de crear y editar 
+
+ let btnEdit=d.querySelector(".btn-updat")
+ btnCreate.classlist.toggle("d-nome");
+ btnEdit.classlist.toggle("d-nome");
+
+//agregar evento al botón editar 
+ btnEdit.addEventlistener("click",()=>{
+ product = {
+ id:productUpdate.id,
+ nombre: nameInput.value,
+ descripcion: descripcionInput.value,
+ precio: precioInput.value,
+ stock: stockInput.value,
+ imagen: imagen.src
+ }
+ // borrar info de localStorage 
+ localStorage.removeItem("productEdit")
+ //pasar los datos del producto a la función
+});
+ };
+ sendDataProduct(product); 
+
+ //función para realizar la petición al servidor 
+
+ let senUpdateProduct= async (pro) =>{
+   let sendDataProduct = async (data) => {
+   let url = "http://localhost/apiCrud/crear-producto"
+   try {
+
+      let respuesta = await fetch(url, {
+   method: "put",
+   headers: {
+      "Content-Type": "application/json"
+   },
+   body: JSON.stringify(data)
+
+   });
+   if (respuesta.status === 406) {
+    alert("los datos enviados no son adminitos");
+
+   }else {  
+    let mensaje = await respuesta.json();
+    alert(mensaje.mensaje);
+    listado.href = "listado-pro.html";
+   }
+
+ }  catch (error) {
+   console.log(error);
+ }
+   }
+}
+ 
--- frontend-apicrud/js/listado-pro.js
+++ frontend-apicrud/js/listado-pro.js
@@ -1,12 +1,30 @@
  //variables globales
  let tablepro  = document.querySelector("#table-pro > tbody");
  let searchInput = document.querySelector ("#search-input")
+ let nameUser = document.querySelector("#nombre-usuario");
+ let btnLogout = document.querySelector("#btnLogout");
 
- //evento para probar el campo de buscar 
- searchInput.addEventListener("keyup",()=>{
-    console.log (searchInput.value);
+  //Funcion para poner el nombre del usuario
+ let getUser = () => {
+   let user = JSON.parse(localStorage.getItem("userLogin"));
+   nameUser.textContent = user.nombre;
+ }
+//evento para el boton del logout
+ btnLogout.addEventListener("click",() =>{ 
+    localStorage.removeItem("userLogin");
+    location.href = "../login.html";
  });
 
+//evento para el input de busqueda 
+ searchInput.addEventListener("keyup",()=>{
+    console.log(searchInput.value);
+ });
+
+ //evento para el navegador
+ document.addEventListener("DOMContentLoaded",()=>{
+    getTableData();
+    getUser();
+ });
  //funcion para traer los datos de la db a la tabla 
 
  let getTableData= async()=>{
@@ -25,7 +43,8 @@ try {
     let tableData = await respuesta.json();
     console.log(tableData);
 
-    //agregar los datos a la tabla
+    //agregar los datos a la tabla localStorage
+    localStorage.setItem("Datostabla", JSON.stringify(tableData));
     tableData.forEach((dato, i) => {
         let row = document.createElement("tr");
         row.innerHTML = `
@@ -39,14 +58,17 @@ try {
             <button id="btn-edit" onclick="editDatable(${dato.id})" type="button" class="btn btn-outline-warning">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
              <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+
             </svg>
             
-            </button> - <button id="btn-delete" onclick="eliminarDatable(${dato.id})" type="button" class="btn btn-outline-danger">
+            </button> - }
+            ${ nameUser.textContent == "vendedor" ? "": `
+            <button id="btn-delete" onclick="eliminarDatable(${dato.id})" type="button" class="btn btn-outline-danger">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
             </svg>
             
-            </button>
+            </button  `}>
             </td> 
         `;
 
@@ -61,13 +83,63 @@ try {
 
   //funcion para editar algun producto de la tabla
   let editDatable = async (pos) => {
+   let product = [];
+   let productsSave=JSON.parse(localStorage.getItem("datosTabla"));
+   if(productsSave != null){
+       products=productsSave;
+   }
+       let singleProduct= products[pos];
+
+       //console.log(singleProducts);
+      localStorage.setItem("productEdit",JSON.stringify(singleProduct));
+      localStorage.removeItem("datosTabla");
+      location.href= "../crear-pro.html";
+
+
   }
-
-
-
 
 
    //funcion para eliminar algun producto de la tabla
    let eliminarDatable = async (pos) => {
+   let product = [];
+   let productsSave=JSON.parse(localStorage.getItem("datosTabla"));
+   if(productsSave != null){
+       products=productsSave;
    }
-   
+       let singleProduct= products[pos];
+       //console.log("producto a eliminar", singleProduct);
+       let IDproduct ={
+        id: singreproduct.id
+       }
+       
+       let confirmar = confirm(`¿Estas seguro de eliminar ${singleProduct.nombre}?`);
+         if (confirmar) {
+            // llamar la funcion para realizar la peticion 
+            senDeleteRequest(IDproduct);
+         }
+   }
+//funcion para realizar la peticion de eliminar producto
+let senDeleteRequest = async (id) => {
+      let url = "http://localhost/apiCrud/crear-producto"
+   try {
+
+      let respuesta = await fetch(url, {
+   method: "DELETE",
+   headers: {
+      "Content-Type": "application/json"
+   },
+   body: JSON.stringify(id)
+   });
+   if (respuesta.status === 406) {
+    alert("el ID enviado no es adminito");
+
+   }else {  
+    let mensaje = await respuesta.json();
+    alert(mensaje.mensaje);
+    location.reload();
+   }
+
+ }  catch (error) {
+   console.log(error);
+ }
+}
